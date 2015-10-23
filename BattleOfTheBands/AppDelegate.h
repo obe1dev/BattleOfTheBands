67f4d70b2640baac36849c695d653f89f80beb50
Brock Oberhansley <oberhansley@me.com>
2015-10-22 19:04:33 -0600

    Initial Commit

---
+++ BattleOfTheBands/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BattleOfTheBands
+//
+//  Created by Brock Oberhansley on 10/22/15.
+//  Copyright © 2015 Brock Oberhansley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BattleOfTheBands/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  BattleOfTheBands
+//
+//  Created by Brock Oberhansley on 10/22/15.
+//  Copyright © 2015 Brock Oberhansley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
