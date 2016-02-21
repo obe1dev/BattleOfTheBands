64aa84ac2d63e3a8b221f0d48d65c39c3221fde9
Brock Oberhansley <brockoberhansley@Brocks-MacBook-Pro.local>
2016-02-21 12:33:40 -0700

    added terms view

---
+++ TermsViewController.h
@@ -0,0 +1,14 @@
+//
+//  TermsViewController.h
+//  BOTB
+//
+//  Created by Brock Oberhansley on 2/21/16.
+//  Copyright © 2016 Brock Oberhansley. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface TermsViewController : UIViewController
+
+
+@end
---
+++ TermsViewController.m
@@ -0,0 +1,42 @@
+//
+//  TermsViewController.m
+//  BOTB
+//
+//  Created by Brock Oberhansley on 2/21/16.
+//  Copyright © 2016 Brock Oberhansley. All rights reserved.
+//
+
+#import "TermsViewController.h"
+
+@interface TermsViewController ()
+
+@end
+
+@implementation TermsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+- (IBAction)backToLogin:(id)sender {
+    [self dismissViewControllerAnimated:true completion:nil];
+    
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+
+@end
